3b7a838b2c85fa73fd1a5c7fe3124d420ee5ebca
t3hmun <t3hmun@gmail.com>
2015-12-20 15:10:56 +0000

    Completed It works

--- Datos/Dupe.cs
+++ Datos/Dupe.cs
@@ -1,4 +1,4 @@
-﻿namespace Datos
+﻿namespace t3hmun.Datos
 {
     using System;
     using System.Collections.Generic;
---
+++ DupeReport/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace t3hmun.Datos
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var cwd = Directory.GetCurrentDirectory();
+            var di = new DirectoryInfo(cwd);
+            var res = Dupe.FindDupesInDir(di);
+            var text = new List<string>();
+            foreach (var group in res)
+            {
+                var names = string.Join("|", group.Select(a => a.File.Name));
+                var pathlines = group.Select(f => " " + f.File.FullName);
+                text.Add(names);
+                text.AddRange(pathlines);
+            }
+
+            File.WriteAllLines("dupes.txt", text);
+        }
+    }
+}
